4654eb46a17b18570230fbcf1a33985ed1221cba
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2022-08-20 20:16:49 +0600

    Add files via upload

---
+++ Calender.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int getFirstDayofTheyear(int year){
+int day = (year*365+((year-1)/4)-((year-1)/100)+((year-1)/400)) %7;
+return day;
+}
+int main(){
+char *months[] = {"January", "February", "March", "April", "May", "June","July", "August", "September", "October", "Novembor","December" };
+int daysInMonth[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+int i,j,totalDays,weekDay = 0,spaceCounter=0,year;
+printf("Enter your favorite Year :\n");
+scanf("%d",&year);
+printf("\n\n******************Welcome to %d ********************\n\n",year);
+//check if it is leap year
+if((year % 4 == 0 && year % 100 !=0) || (year % 400 == 0)){
+daysInMonth[1] = 29;
+}
+//get the first day of year
+weekDay = getFirstDayofTheyear(year);
+for(i=0;i<12;i++){
+   printf("\n\n\n------------------- %s---------------------\n",months[i]);
+
+   printf("\n   Sun   Mon   Tue   Wed   Tue   Fri   Sat\n\n");
+for(spaceCounter = 1;spaceCounter <=weekDay; spaceCounter++){
+  printf("      ");
+
+}
+   totalDays =daysInMonth[i];
+
+for(j=1; j<=totalDays; j++){
+
+
+    printf("%6d",j);
+    weekDay++;
+    if(weekDay>6){
+
+        weekDay=0;
+        printf("\n");
+    }
+}
+
+
+}
+
+
+
+return 0;
+}
+
